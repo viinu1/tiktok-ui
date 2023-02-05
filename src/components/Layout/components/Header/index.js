9d93d13a2e52fc4317d7954173164032c756fdab
Vĩ Phạm Văn <phamviabc2@gmail.com>
2023-02-05 22:04:13 +0700

    Xây dụng UI Header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,10 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark,faSpinner, faMagnifyingGlass,faPlus } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss'
+import images from '~/assets/images';
+
 
 const cx = classNames.bind(styles)
 
@@ -7,7 +12,30 @@ function Header() {
     return ( 
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
+                <div>
+                    <img src={images.logo} alt="Tiktok"/>
+                </div>
 
+                <div className={cx('search')}>
+                    <input type='text' placeholder='Searh Account and Video' spellCheck={false}/>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark}/>
+                    </button>
+                    <FontAwesomeIcon className={cx('spinner')} icon={faSpinner}/>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                    </button>
+                </div>
+
+                <div className={cx('actions')}>
+                    <button>
+                        <FontAwesomeIcon icon={faPlus}/>
+                        Tải lên
+                    </button>
+                    <button>
+                        Đăng nhập
+                    </button>
+                </div>
             </div>
         </header>
      );

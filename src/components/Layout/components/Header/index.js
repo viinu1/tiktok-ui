439a5498aadb1bfe6051de531fb916050bb5a6f2
Vĩ Phạm Văn <phamviabc2@gmail.com>
2023-02-06 18:41:01 +0700

    Xây dựng select option

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark,faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark,faSpinner, faMagnifyingGlass, faEllipsisVertical, faLanguage, faQuestionCircle, faKeyboard } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
+
 import 'tippy.js/dist/tippy.css'; // optional
 
 import styles from './Header.module.scss'
@@ -10,13 +11,32 @@ import images from '~/assets/images';
 import { Wrapper as PopperWrapper} from '~/components/Popper'
 import AccountItem from '~/components/AccountItem';
 import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
 
 
 
 const cx = classNames.bind(styles)
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon ={faLanguage}/>,
+        title:'English',
+    },
+    {
+        icon:<FontAwesomeIcon icon={faQuestionCircle}/>,
+        title:'Feedback and help',
+        to:'/feedback'
+    },
+    {
+        icon:<FontAwesomeIcon icon={faKeyboard}/>,
+        title:'Keyboard shortcuts',
+    },
+]
 
 function Header() {
-
+    // const iconOption = cx('option-icon')
+    // tippy('iconOption',{
+    //     content:'hello'
+    // })
     const [searchResult,setSearchResult] = useState([])
     useEffect(()=>{
         setTimeout(()=>{
@@ -33,8 +53,9 @@ function Header() {
 
                 <Tippy
                     interactive
+                    placement='bottom'
                     visible={searchResult.length > 0}
-                    render={attrs => (
+                    render={(attrs) => (
                         <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                                 <PopperWrapper>
                                     <h3 className={cx('search-title')}>
@@ -42,8 +63,7 @@ function Header() {
                                     </h3>
                                     <AccountItem/>
                                     <AccountItem/>
-                                    <AccountItem/>
-                            
+                                    <AccountItem/>                                  
                                 </PopperWrapper>
                             </div>
                       )}
@@ -63,6 +83,14 @@ function Header() {
                 <div className={cx('actions')}>
                     <Button text>Upload</Button>
                     <Button primary>Login</Button>
+                    
+                    <Menu items = {MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical}/>
+                        </button>
+                    </Menu>
+                    
+
                 </div>
             </div>
         </header>
---
+++ src/components/Popper/Menu/MenuItem.js
@@ -0,0 +1,20 @@
+import classNames from "classnames/bind";
+import styles from './Menu.module.scss'
+
+import Button from "~/components/Button";
+
+const cx =classNames.bind(styles)
+
+function MenuItem({data}) {
+    return (
+        <Button
+            to = {data.to}
+            leftIcon={data.icon}
+            className={cx('menu-item')}
+        >
+            {data.title}
+        </Button>
+    );
+}
+
+export default MenuItem;
